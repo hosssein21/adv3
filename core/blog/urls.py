775a8c7fb42f70b5957c240634c03e673ac8b4a5
hosssein <hossseinkazemi79@gmail.com>
2023-12-02 22:02:54 +0330

    cbv complete

---
+++ core/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name='blog'
+urlpatterns = [
+    path('index/',views.IndexView.as_view(),name='index'),
+    path('redirect/',views.Redirect.as_view(),name='redirect'),
+    path('posts/',views.PostList.as_view(),name='posts'),
+    path('post/<int:pk>/',views.PostDetail.as_view(),name='post'),
+    path('post/create/',views.PostCreate.as_view(),name='create'),
+    path('post/update/<int:pk>/',views.PostUpdate.as_view(),name='update'),
+    path('post/delete/<int:pk>/',views.PostDelete.as_view(),name='delete'),
+]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,45 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import TemplateView,RedirectView,ListView,DeleteView,CreateView,UpdateView,DetailView
+from .models import Post
 
-# Create your views here.
+class IndexView(TemplateView):
+    
+    template_name='blog/index.html'
+    
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context["name"]="hosssein"
+        return context
+    
+class Redirect(RedirectView):
+    url = 'http://google.com/'
+    
+class PostList(ListView):
+    template_name="blog/posts.html"
+    model=Post
+    context_object_name="posts"
+    
+class PostDetail(DeleteView):
+    template_name="blog/post.html"
+    queryset=Post.objects.filter(Active=1)
+    
+class PostCreate(CreateView):
+    template_name="blog/create.html"
+    model=Post
+    fields=["title","content","author","category"]
+    success_url="/blog/posts/"
+    
+class PostUpdate(UpdateView):
+    template_name="blog/create.html"
+    model=Post
+    fields=["title","content"]
+    success_url="/blog/posts/"
+    
+class PostDelete(DeleteView):
+    
+    template_name="blog/delete.html"
+    model=Post
+    success_url="/blog/posts/"
+    
+
